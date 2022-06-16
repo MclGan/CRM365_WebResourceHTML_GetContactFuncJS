c035428d2e8cb2a1b48d73ee53a2886450f772c0
Michael Galkin <mclgalkin@gmail.com>
2022-06-16 21:27:42 +0300

    Lets roll

---
+++ GetContactJSWebResourceCRM.js
@@ -0,0 +1,27 @@
+﻿function GetContacts() {
+    debugger;
+
+    window.parent.Xrm.WebApi.online.retrieveMultipleRecords("contact", "?$select=_accountid_value,fullname").then(
+        function success(results) {
+            //debugger;
+            for (var i = 0; i < results.entities.length; i++) {
+                var _accountid_value = results.entities[i]["_accountid_value"];
+                var _accountid_value_formatted = results.entities[i]["_accountid_value@OData.Community.Display.V1.FormattedValue"];
+                var _accountid_value_lookuplogicalname = results.entities[i]["_accountid_value@Microsoft.Dynamics.CRM.lookuplogicalname"];
+                var fullname = results.entities[i]["fullname"];
+
+                var table = document.getElementById("IrakTable");
+                var row = table.insertRow();
+                var cell = row.insertCell();
+                cell.innerHTML = _accountid_value + " " + _accountid_value_formatted + " " + _accountid_value_lookuplogicalname;
+                cell = row.insertCell();
+                cell.innerHTML = fullname;
+            }
+
+        },
+        function (error) {
+            Xrm.Utility.alertDialog(error.message);
+        }
+    );
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # CRM365_WebResourceHTML_GetContactFuncJS
+
+## Description
+
+CRM Dynamics 365
+- Web Resource html для вывода списка контактов на форме 
+- JS скрипт для подгрузки контактов
